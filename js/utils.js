2ea1c46757a8044b717edb0c8f968259fab46554
LukovDev <ruslanfelitsan@gmail.com>
2024-05-30 20:35:06 +0300

    commit 30 may 2024

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+
+// Получить текущий год:
+function get_current_year() {
+    document.write(new Date().getFullYear());
+}
+
+
+// Перейти по ссылке:
+function link_href(link) {
+    window.location.href = link;
+}
